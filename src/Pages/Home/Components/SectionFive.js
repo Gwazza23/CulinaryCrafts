c8c01ebf3b3765fd5cdd7f1b6f5bcfe789172a3e
Gwazza23 <weerawarnagayan@gmail.com>
2023-10-17 01:03:04 +0100

    Create Section Five Component for Home Page

---
+++ src/Pages/Home/Components/SectionFive.js
@@ -0,0 +1,64 @@
+import { useState } from "react";
+import "./SectionFive.css";
+import axios from "axios";
+import { Link } from "react-router-dom";
+
+function SectionFive() {
+  const [data, setData] = useState(false);
+  function handleRandom() {
+    axios
+      .get(
+        "https://www.themealdb.com/api/json/v1/1/random.php?app_id=bff6806a&app_key=%20d5afbba359e71e8e1911a2ba473ca264"
+      )
+      .then((response) => {
+        setData(response);
+      });
+  }
+  console.log(data);
+  return (
+    <div className="section-five-container">
+      <img id="section-five-img1" src="/media/section-five-img1.webp" alt=" " />
+      <img id="section-five-img2" src="/media/section-five-img2.webp" alt=" " />
+      <div className="section-five-header">
+        <h2>Surprise Your Taste Buds!</h2>
+      </div>
+      <div className="section-five-main">
+        {data ? (
+          <div className="section-five-pt1">
+            <img
+              src={data.data?.meals[0].strMealThumb}
+              alt={data.data?.meals[0].strMeal}
+              loading="lazy"
+            />
+            <div className="section-five-info">
+              <h3>{data.data?.meals[0].strMeal}</h3>
+              <p>
+                Cuisine: <span>{data.data?.meals[0].strArea}</span>
+              </p>
+              <p>
+                Dish Type: <span>{data.data?.meals[0].strCategory}</span>
+              </p>
+              <Link>Get The Recipe!</Link>
+            </div>
+          </div>
+        ) : (
+          <div className="section-five-pt1-initial">
+            <h2>?</h2>
+          </div>
+        )}
+        <div className="section-five-pt2">
+          <p>
+            Ready to embark on a culinary adventure? Click the "Random Recipe"
+            button below and let your taste buds journey through our diverse
+            collection of mouthwatering dishes. With a simple click, you'll
+            uncover delightful recipes that'll satisfy your cravings and inspire
+            your next cooking endeavor
+          </p>
+          <button onClick={handleRandom}>Random Recipe</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default SectionFive;
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -3,12 +3,14 @@ import SectionOne from "./Components/SectionOne";
 import SectionTwo from "./Components/SectionTwo";
 import SectionThree from "./Components/SectionThree";
 import SectionFour from "./Components/SectionFour";
+import SectionFive from './Components/SectionFive';
 
 function Home() {
   return (
     <>
       <SectionOne />
       <SectionTwo />
+      <SectionFive />
       <SectionThree />
       <SectionFour />
     </>
